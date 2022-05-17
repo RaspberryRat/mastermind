d2d1b8057e9047e3f2a8a6ef36bb7098f15ebd3f
Graham <git@grahamirvine.com>
2022-05-17 15:58:06 -0600

    Have ComputerPlayer create random starting code

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,11 @@
 require "pry-byebug" # cause I know I'll need it
 
+module Codes
+  CODES = %w[red green blue yellow brown orange black white].freeze
+end
+
 class Game
+  include Codes
   def initialize
     @board = Array.new(4)
     @player1 = HumanPlayer.new
@@ -8,11 +13,15 @@ class Game
   end
   attr_reader :board
 
+  def self.code_maker # shouldn't be a class method. Need to figure this out
+    @board = Array.new(4) # need to get this to pull properly from initialize not re-create
+    @board.map! { |code| CODES.sample }
+  end
 end
 
 class Player
   def initialize
-    @game = game
+  #  @game = game
   end
 end
 
@@ -22,7 +31,7 @@ class HumanPlayer < Player
     name = gets.chomp.to_s
     @name = name
   end
-  
+
   def guess_code
     puts "Make your guess"
   end
@@ -31,7 +40,8 @@ end
 class ComputerPlayer < Player
   def create_code
     # create the code here
+    Game.code_maker # don't want this to call a class method. Need to figure out how to call instance method
   end
 end
 
-
+puts ComputerPlayer.new.create_code
