4e5093c6ee6dd5c81d2c834a6702f4e65bd3563f
Graham <git@grahamirvine.com>
2022-05-26 16:46:08 -0600

    guesses are working. Seems to break code after 9 rounds consistenly

--- mastermind.rb
+++ mastermind.rb
@@ -380,20 +380,35 @@ class ComputerPlayer < Player
     current_colour_feedback = past_feedback[round-1][1]
     puts "This is the current feedback: #{current_feedback}"
     puts "This is current colour feedback : #{current_colour_feedback}"
-    ##binding.pry
+    #binding.pry
     update_guesses(current_feedback, current_guess)
   end
 
   def update_guesses(feedback, guess)
-    ##binding.pry
+    #binding.pry
     round = @game.round_number
     to_delete = []
     to_keep = []
     to_delete.push(guess)
-    feedback_w_colour = feedback + @past_feedback[@game.round_number - 1][1]
-    ##binding.pry
+    feedback_w_colour = feedback + @past_feedback[round - 1][1]
+    #binding.pry
     if feedback_w_colour > 0 #i think this is right
-      guess_combination_check(feedback, guess)
+      guess_combination_check(feedback_w_colour, guess)
+    end
+    if feedback_w_colour > 0 && feedback == 0
+      #binding.pry
+      i = 0
+      guess.length.times do
+        @possible_guesses.map do |arr|
+          j = 0
+          arr.length.times do
+            to_delete.push(arr) if arr[j] == guess[j]
+            j += 1
+          end
+        end
+        i += 1
+      end
+      #binding.pry
     end
     if feedback_w_colour == 0
       i = 0
@@ -407,14 +422,14 @@ class ComputerPlayer < Player
     previous_feedback = @past_feedback[-2][0].length
     previous_guess = colours_to_numbers(@past_guesses[-2])
       if round > 1 && (feedback < previous_feedback) && @skip == 0
-        ##binding.pry
+        #binding.pry
         if find_index_difference.length == 1
           index = find_index_difference.pop
           @possible_guesses.map do |arr|
             to_keep.push(arr) if arr[index] == previous_guess[index]
           end
         end
-        ##binding.pry
+        #binding.pry
       elsif round > 1 && (feedback == previous_feedback)
         if find_index_difference.length == 1
           index = find_index_difference.pop
@@ -445,13 +460,29 @@ class ComputerPlayer < Player
 
   def guess_combination_check(feedback, guess)
     #binding.pry
-    feedback += @past_feedback[@game.round_number - 1][1]
     i = 0
     correct_guesses = []
     to_keep = []
     guess.combination(feedback) { |g| correct_guesses.push(g) }
     guess.combination(feedback) { |g| correct_guesses.push(g.reverse) }
+    # add multiple shuffles to ensure all orders are included. needs refactor
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
+    guess.combination(feedback) { |g| correct_guesses.push(g.shuffle) }
 
+    correct_guesses.uniq!
     correct_guesses.length.times do
       @possible_guesses.map do |arr|
         arr.combination(feedback) do |n|
@@ -463,12 +494,12 @@ class ComputerPlayer < Player
       i += 1
     end
     @possible_guesses = to_keep.uniq
-    ##binding.pry
+    ####binding.pry
   end
 
   # returns index location of the number that changed between guesses
   def find_index_difference
-    ##binding.pry
+    ####binding.pry
     current_feedback = @past_feedback[-1][0].length
     previous_feedback = @past_feedback[-2][0].length
     current_guess = colours_to_numbers(@past_guesses[-1])
